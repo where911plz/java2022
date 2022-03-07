2b3db99467874e77211a0b719fd923b783c19280
Jacob <w0nse0k@naver.com>
2022-03-07 18:55:15 +0900

    comment 추가

--- src/main/java/kr/mjc/jacob/basics/exception/MyIOException.java
+++ src/main/java/kr/mjc/jacob/basics/exception/MyIOException.java
@@ -1,5 +1,8 @@
 package kr.mjc.jacob.basics.exception;
 
+/**
+ * IOException의 unchecked exception 타입
+ */
 public class MyIOException extends RuntimeException {
 	public MyIOException() {
 		super();
