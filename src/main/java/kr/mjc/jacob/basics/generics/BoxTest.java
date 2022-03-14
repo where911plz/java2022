3f2de57006e64b799fa1c9d6ca5b3a9ed218edde
Jacob <w0nse0k@naver.com>
2022-03-14 18:45:43 +0900

    autoboxing과 unboxing 내용 삭제

--- src/main/java/kr/mjc/jacob/basics/generics/BoxTest.java
+++ src/main/java/kr/mjc/jacob/basics/generics/BoxTest.java
@@ -9,11 +9,6 @@ public class BoxTest {
     stringBox.set("abc");
     String s = stringBox.get();
 
-    // autoboxing: primitive value를 wrapper class의 object로 변환
-    Integer a = 5;
-    // unboxing: wrapper class object를 primitive value로 변환
-    int b = a;
-
     IntegerBox integerBox = new IntegerBox();
     integerBox.set(5);
     int i = integerBox.get();
