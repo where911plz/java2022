5bab4de3ac8a7b843c3e1139315c21a293aba134
Jacob <w0nse0k@naver.com>
2022-03-24 11:12:56 +0900

    Lombok 적용

--- src/main/java/kr/mjc/jacob/basics/collections/Student.java
+++ src/main/java/kr/mjc/jacob/basics/collections/Student.java
@@ -1,43 +1,18 @@
 package kr.mjc.jacob.basics.collections;
 
-import java.util.Objects;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
 
 /**
  * p.418 예제 7-7. 학생 클래스
  */
+@AllArgsConstructor
+@Getter
+@ToString
+@EqualsAndHashCode
 public class Student {
-  private int id;
-  private String name;
-
-  public Student(int id, String name) {
-    this.id = id;
-    this.name = name;
-  }
-
-  public int getId() {
-    return id;
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  @Override
-  public String toString() {
-    return "Student{" + "id=" + id + ", name='" + name + '\'' + '}';
-  }
-
-  @Override
-  public boolean equals(Object o) {
-    if (o instanceof Student) {
-      Student s = (Student) o;
-      return this.id == s.getId() && Objects.equals(this.name, s.getName());
-    }
-    return false;
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(id, name);
-  }
+	private int id;
+	private String name;
 }
