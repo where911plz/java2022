84fc81667019b6b236eec6601e179189ff809fa2
Jacob <w0nse0k@naver.com>
2022-03-29 20:23:08 +0900

    @FunctionalInterface 삽입

--- src/main/java/kr/mjc/jacob/basics/jdbc/ResultMapper.java
+++ src/main/java/kr/mjc/jacob/basics/jdbc/ResultMapper.java
@@ -3,6 +3,7 @@ package kr.mjc.jacob.basics.jdbc;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+@FunctionalInterface
 public interface ResultMapper<T> {
   T map(ResultSet rs) throws SQLException;
 }
